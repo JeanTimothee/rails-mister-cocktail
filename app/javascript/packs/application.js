abb3df1e1e85a57cc74d92fd93894a30ccd2ce9b
Timothee Regis <registimothee@gmail.com>
2020-02-13 20:58:47 +0100

    started front

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'select2/dist/css/select2.css';
+
+import { initSelect2 } from '../plugins/init_select2';
+initSelect2();
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
